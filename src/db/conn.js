f48b9c8ccda42dd82247c97a7b5b182706c2896e
github-srijoyee <137808302+github-srijoyee@users.noreply.github.com>
2024-01-11 00:15:23 +0530

    loading atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,17 @@
-const mongoose=require("mongoose");
+/*const mongoose=require("mongoose");
 
 //creating a database
-mongoose.connect('mongodb://127.0.0.1:27017/photography',/*{
-    useCreateIndex:true, //not used now renders error
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}*/).then(()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/photography',).then(()=>{
     console.log('connection successful');
 }).catch((error)=>{
     console.log(error);
+})*/
+
+const mongoose=require("mongoose");
+const DB='mongodb+srv://bhanjasrijoyee2003:j0yee@cluster1.8ew2vo6.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB).then(()=>{
+    console.log(`connection successful`);
+}).catch((error)=>{
+    console.log(`no connection`);
 })
